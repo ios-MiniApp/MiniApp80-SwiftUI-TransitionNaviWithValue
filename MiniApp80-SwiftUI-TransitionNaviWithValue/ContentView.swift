d2ee7646d5c2aacf25a8d002adf2cc37b5e5f4fa
Kota <maedakoutaa@gmail.com>
2022-08-14 10:17:59 +0900

    Initial Commit

---
+++ MiniApp80-SwiftUI-TransitionNaviWithValue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp80-SwiftUI-TransitionNaviWithValue
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp80-SwiftUI-TransitionNaviWithValue/MiniApp80_SwiftUI_TransitionNaviWithValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp80_SwiftUI_TransitionNaviWithValueApp.swift
+//  MiniApp80-SwiftUI-TransitionNaviWithValue
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp80_SwiftUI_TransitionNaviWithValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
